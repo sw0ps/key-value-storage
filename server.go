862b1b5a6fa5282ec416b7d5d2863120ff7a2cd8
sw0ps <dimausachovwv@gmail.com>
2022-08-15 22:42:04 +0300

    add keyValuePutHandler for create

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 
@@ -8,15 +9,37 @@ import (
 )
 
 const port = "8080"
+const mainUrlPath = "/v1/{key}"
 
 func main() {
 	r := mux.NewRouter()
 
-	r.HandleFunc("/", helloGoHandler)
+	r.HandleFunc(mainUrlPath, keyValuePutHandler).Methods(http.MethodPut)
 
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
-func helloGoHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello http!"))
+// keyValuePutHandler Example:
+// 	curl -X PUT -d 'Hello, key-value store!' -v http://localhost:8080/v1/key-a
+
+// keyValuePutHandler - create key/value by PUT-request
+func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // get key from request
+	key := vars["key"]
+
+	value, err := io.ReadAll(r.Body) // request's body with value
+	defer r.Body.Close()
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = Put(key, string(value))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
 }
