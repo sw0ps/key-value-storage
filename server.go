5650fd436d7a80957fca159d9b39e4a40224f8b4
sw0ps <dimausachovwv@gmail.com>
2022-08-18 19:54:28 +0300

    add get and delete handlers

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"io"
 	"log"
 	"net/http"
@@ -15,14 +16,17 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc(mainUrlPath, keyValuePutHandler).Methods(http.MethodPut)
+	r.HandleFunc(mainUrlPath, keyValueGetHandler).Methods(http.MethodGet)
+	r.HandleFunc(mainUrlPath, keyValueDeleteHandler).Methods(http.MethodDelete)
 
+	log.Printf("Server started at port %v", port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
 
 // keyValuePutHandler Example:
 // 	curl -X PUT -d 'Hello, key-value store!' -v http://localhost:8080/v1/key-a
 
-// keyValuePutHandler - create key/value by PUT-request
+// keyValuePutHandler - create key value using PUT-request
 func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r) // get key from request
 	key := vars["key"]
@@ -42,4 +46,41 @@ func keyValuePutHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.WriteHeader(http.StatusCreated)
+}
+
+// keyValueGetHandler Example:
+//	curl -X GET -v http://localhost:8080/v1/key-a
+
+// keyValueGetHandler - get value by key using GET-request
+func keyValueGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // get key from request
+	key := vars["key"]
+
+	value, err := Get(key)              // get value by key
+	if errors.Is(err, ErrorNoSuchKey) { // check for 404
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(value)) // write value to response
+}
+
+// keyValueDeleteHandler Example:
+//	curl -X DELETE -v http://localhost:8080/v1/key-a
+
+// keyValueDeleteHandler - delete value by key using DELETE-request
+func keyValueDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r) // get key from request
+	key := vars["key"]
+
+	err := Delete(key) // delete value by key
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
